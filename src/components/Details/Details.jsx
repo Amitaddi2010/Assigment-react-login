b883d8c4327588908a1c12d423815441b57a9e63
Amit Raj Saraswat <Amit.addi2010@gmail.com>
2022-05-19 00:42:38 +0530

    level1

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,6 +1,7 @@
 import "./Details.css"
 import logo from '../../images/apple.webp'
 import React from 'react'
+import Login from "./../Login/Login"
 
 function Details({ user }) {
   return (
@@ -8,28 +9,65 @@ function Details({ user }) {
       <nav className="details-nav" style={{gridArea:'nav'}}>
         <img src={logo} alt="" height={50} />
         <span style={{ textAlign: 'center', display: 'table-cell', lineHeight: '3rem' }}>{user}</span>
-        <button>Logout</button>
+        <button onClick={Login}>Logout</button>
       </nav>
       <aside className="details-sidebar" style={{gridArea:'sidebar'}}>
         Dashboard
       </aside>
       <form className="details-form" action="post" style={{gridArea:'form'}}>
-        <div style={{textAlign:"left"}}>Add Employee</div>
+        <div style={{textAlign:"left"}} >Add Employee</div>
         <hr />
         <div>Details for Appointment Letter</div>
         <hr />
         <div className="details-form-fields">
 
-          <label htmlFor="username">Username :</label>
+          <label htmlFor="Adhaar">Adhaar No :</label>
+          <input
+            type="number"
+            name='ADhaar'
+            placeholder="Enter Adhaar No" />
+  
+          <label htmlFor="FullName">FullName :</label>
           <input
             type="text"
-            name='username' />
-  
-          <label htmlFor="password">Password :</label>
+            name='FullName' 
+            placeholder="Enter FullName"/>
+            <label htmlFor="FatherName">Father Name :</label>
           <input
-            type="password"
-            name='password' />
-        <span style={{gridColumn:'1 / span 2'}}><button type='submit'>Login now</button></span>
+            type="text"
+            name='FatherName'
+            placeholder="Enter Father name" />
+  
+          <label htmlFor="Address">Address :</label>
+          <input
+            type="text"
+            name='Address'
+            placeholder="Enter Address" />
+            <label htmlFor="City">City:</label>
+          <input
+            type="text"
+            name='City'
+            placeholder="Enter City" />
+  
+          <label htmlFor="Mob Number">Mobile Number :</label>
+          <input
+            type="number"
+            name='Mobile Number'
+            placeholder="Enter Mobile Number" />
+             <label htmlFor="Adhaar Card Upload">Adhaar Card Upload : </label>
+            <div class="upload-btn-wrapper">
+               < button class="btn">Upload a file</button>
+            <input type="file" name="myfile" />
+              </div>
+              <label htmlFor="Photo">Photo Upload : </label>
+            <div class="upload-btn-wrapper">
+            < button class="btn">Upload a file</button>
+            <input type="file" name="myfile" />
+              </div>
+        <span style={{gridColumn:'1 / span 2'}}>
+       
+        < button class="btn2" onClick={alert}>Submit</button>
+        </span>
 </div>
       </form>
     </section>
